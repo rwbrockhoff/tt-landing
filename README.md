cdab28f4674bae8e9bc728a75721e3270be2f01c
rwbrockhoff <brockhoffrw@gmail.com>
2024-04-06 10:52:22 -0600

    feat: initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🥾 Tidytrek Landing Page
+
+## Welcome to Tidytrek!
+
+This is the intial documentation for Tidytrek's landing page.
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "Tidytrek";
+export const SITE_DESCRIPTION =
+  "Pack organization for backpackers and travelers.";
