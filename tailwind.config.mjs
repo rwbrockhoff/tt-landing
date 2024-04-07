b0db9aa28d71927322e481d9c1e220ee21eb2aff
rwbrockhoff <brockhoffrw@gmail.com>
2024-04-06 21:18:02 -0600

    style: extend primary color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: '#f2fbf8',
+					100: '#d1f6ea',
+					200: '#9eead4',
+					300: '#6edabf',
+					400: '#40c1a5',
+					500: '#27a58b',
+					600: '#1d8471',
+					700: '#1b6a5c',
+					800: '#1a554b',
+					900: '#1a4740',
+					950: '#092a26',
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
