91ff0e0b511e30a24e31e69d83b6053d4b3e371e
rwbrockhoff <brockhoffrw@gmail.com>
2025-08-05 18:29:14 -0600

    feat: terms of service, cookie banner, finish marketing copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tidytrek.co',
-	base: '/',
+	base: process.env.PUBLIC_BASE_PATH || '/',
 });
